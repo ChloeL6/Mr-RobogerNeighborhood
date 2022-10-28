7ac08149cd47244360bdbad61391b8520e6edd1e
Chloe Le <yenchi.le1199@gmail.com>
2022-10-28 13:07:04 -0700

    Modify function on test 1 to clear it

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 
 Describe : beepboop()
 
-Test 1: "Add 0 to UserInput
+Test 1: "Add 0 to user input
 Code:
-const userInput = [4];
+const userInput = 5;
 const startNum = [0];
-function beepBoop() 
-Expected Output: [0,4]
+function beepBoop()
+Expected Output: [0,5]
 
-Test 2:
+Test 2: 
 Code:
-Expected Output
+Expected Output [0,1,2,3,4]
 
 Test 2:
 Code:
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-const userInput = [4];
-//let result = [];
+const userInput = 5;
 const startNum = [0];
 
 function beepBoop() {
@@ -8,9 +7,31 @@ function beepBoop() {
 }
 
 
-function userInputToString (startNum, userInput) {
-  for (let i = startNum; i <= userInput; i+=1 )
-  return result = result.push(i);
+//Test 2
+
+let userInput = 5;
+
+let numArr = [];
+
+function beepBoop() {
+for( let i = 0; i <= 5; i++) {
+  numArr.push(i);
+}
+return numArr;
+}
+
+const userInput = 5;
+const startNum = [0];
+let numArr = [];
+
+function beepBoop() {
+  newUserInput = startNum.concat(userInput);
+  for( let i = 0; i <= 5; i++) {
+    numArr.push(i);
+  }
+  return numArr;
+  }
+ 
 }
 
 
@@ -18,8 +39,27 @@ function userInputToString (startNum, userInput) {
 
 
 
+  for (let i = startNum; i <= userInput; i+=1 ) {
+    result.push(i);
+    console.log(i);
+  }
+  return result;
+}
+
+const userInput = [4];
+let firstIndex = 0;
+while (firstIndex <= userInput) {
+  cons
+}
 
 
+
+
+
+
+function userInputToString (startNum, userInput) {
+  for (let i = startNum; i <= userInput; i+=1 )
+  return result = result.push(i);
 
 
 function beepBoop() {
