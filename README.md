c947c6c3fec3ee84e6f75eb3b6f6f77a2c83344a
Chloe Le <yenchi.le1199@gmail.com>
2022-10-28 10:30:44 -0700

    Modifying test 1 due to misunderstanding the prompt

--- README.md
+++ README.md
@@ -1,24 +1,9 @@
 
 Describe : beepboop()
 
-Test 1: "It should return an array of number start with 0 from user input"
+Test 1: 
 Code:
-const userInput = [1, 2, 3, 4]
-function beepBoop() 
-Expected Output: [0, 1, 2, 3, 4]
-
-
-Test 2: "Replace 1 with "Beep!"
-Code:
-const userInput = [1, 2, 3, 4]
-function beepBoop()
-Expected Output: [0, "Beep!", 2, 3, 4]
-
-Test 3: "Check if userInput has a 1"
-Code:
-const userInput = [1, 2, 3, 4, 11]
-function beepBoop()
-Expected Output: true
+Expected Output:
 
 Test 2:
 Code:
--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,10 @@
-const userInput = [1, 2, 3, 4]; //Test 1
-const firstIndex = [0]
-let newUserInput; 
+const userInput = (4);
+const result = [];
+const startNum = (0);
 
-function beepBoop() {
-  return newUserInput = firstIndex.concat(userInput);
-
-
-
-
-
-
-
-
-
-
-
-
-
-function beepBoop() {
-  newUserInput = firstIndex.concat(userInput);
-for (i = 0; i < userInput.length; i +=1) {
-  if ( userInput[i] === 1) {
-    console.log("looped");
-  return true;
-    };
-  }
+function userInputToString (startNum, userInput) {
+  for (let i = startNum; i <= userInput; i+=1 )
+  return result.push(i);
 }
 
 
@@ -35,35 +15,6 @@ for (i = 0; i < userInput.length; i +=1) {
 
 
 
-
-
-}
-
-/*Test 2: "Replace 1 with "Beep!"
-Code:
-const userInput = [1, 2, 3, 4]
-function beepBoop()
-Expected Output: [0, "Beep!", 2, 3, 4]*/
-
-const userInput = [1, 2, 3, 4];
-const firstIndex = [0]
-let newUserInput; 
-
-function beepBoop() {
-  newUserInput = firstIndex.concat(userInput);
-  newUserInput[1] = "Beep!";
-  return newUserInput;
-}
-
-/*Test 3: "For numbers that contain a 1, all digits are replaced with "Beep!"
-Code:
-const userInput = [1, 2, 3, 4, 11]
-function beepBoop()
-Expected Output: [0, "Beep!", 2, 3, 4, "Beep"]*/
-
-const userInput = [1, 2, 3, 4];
-const firstIndex = [0]
-let newUserInput; 
 
 function beepBoop() {
   newUserInput = firstIndex.concat(userInput);
